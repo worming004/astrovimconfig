04ca4db812ba1e6858f0771054b80b9314dd0083
Mathieu Scolas <worming@hotmail.be>
2022-07-05 08:16:05 +0200

    remove already set mapping

--- init.lua
+++ init.lua
@@ -7,7 +7,17 @@ return {
     set.relativenumber = true
 
     -- Set key bindings
-    map("n", "<C-s>", ":w!<CR>", opts)
+    map("o", "&", "1", opts)
+    map("o", "é", "2", opts)
+    map("o", "\"", "3", opts)
+    map("o", "'", "4", opts)
+    map("o", "(", "5", opts)
+    map("o", "§", "6", opts)
+    map("o", "è", "7", opts)
+    map("o", "!", "8", opts)
+    map("o", "ç", "9", opts)
+    map("o", "à", "0", opts)
+
     map("n", "&", "1", opts)
     map("n", "é", "2", opts)
     map("n", "\"", "3", opts)
