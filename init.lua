e348b42ab84342c44ec76804d077818e42c2e6b4
Mathieu Scolas <worming@hotmail.be>
2022-07-22 13:35:40 +0200

    indent

--- init.lua
+++ init.lua
@@ -13,28 +13,28 @@ return {
         -- Set options
         set.relativenumber = true
 
-    -- Set key bindings
-    map("o", "&", "1", opts)
-    map("o", "é", "2", opts)
-    map("o", "\"", "3", opts)
-    map("o", "'", "4", opts)
-    map("o", "(", "5", opts)
-    map("o", "§", "6", opts)
-    map("o", "è", "7", opts)
-    map("o", "!", "8", opts)
-    map("o", "ç", "9", opts)
-    map("o", "à", "0", opts)
+        -- Set key bindings
+        map("o", "&", "1", opts)
+        map("o", "é", "2", opts)
+        map("o", "\"", "3", opts)
+        map("o", "'", "4", opts)
+        map("o", "(", "5", opts)
+        map("o", "§", "6", opts)
+        map("o", "è", "7", opts)
+        map("o", "!", "8", opts)
+        map("o", "ç", "9", opts)
+        map("o", "à", "0", opts)
 
-    map("n", "&", "1", opts)
-    map("n", "é", "2", opts)
-    map("n", "\"", "3", opts)
-    map("n", "'", "4", opts)
-    map("n", "(", "5", opts)
-    map("n", "§", "6", opts)
-    map("n", "è", "7", opts)
-    map("n", "!", "8", opts)
-    map("n", "ç", "9", opts)
-    map("n", "à", "0", opts)
+        map("n", "&", "1", opts)
+        map("n", "é", "2", opts)
+        map("n", "\"", "3", opts)
+        map("n", "'", "4", opts)
+        map("n", "(", "5", opts)
+        map("n", "§", "6", opts)
+        map("n", "è", "7", opts)
+        map("n", "!", "8", opts)
+        map("n", "ç", "9", opts)
+        map("n", "à", "0", opts)
 
         -- Set autocommands
         vim.cmd [[
